369eb9cbfb2d102f6477a7e2ec385982e02f60c5
Kardamitsis Evan <kardamitsis.e@gmail.com>
2025-03-10 12:35:46 +0200

    Refactor footer layout and update social media links

--- src/app/sections/Footer/FooterBottom/FooterBottom.tsx
+++ src/app/sections/Footer/FooterBottom/FooterBottom.tsx
@@ -4,9 +4,9 @@ const FooterBottom = () => {
   return (
     <div
       id="contact"
-      className="min-h-[28vh] mt-16 sm:mt-32 flex flex-col items-center justify-center bg-black/90 p-6 sm:p-12 backdrop-blur-md shadow-md"
+      className="mt-16 sm:mt-32 flex flex-col items-center justify-center bg-black/90 p-6 sm:p-12 backdrop-blur-md shadow-md"
     >
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 text-white text-sm w-full max-w-5xl px-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 text-white text-sm w-full max-w-5xl px-4">
         {/* Left Column */}
         <div className="flex flex-col items-center lg:items-start space-y-4">
           <div>
@@ -14,14 +14,9 @@ const FooterBottom = () => {
               info@musicspeaks.gr
             </a>
           </div>
-          <div>
-            <a href="tel:+1234567890" className="font-bold">
-              +123 456 7890
-            </a>
-          </div>
           <div className="flex space-x-4">
             <a
-              href="https://instagram.com"
+              href="https://www.instagram.com/p/DG-xNI7sXWs/?igsh=cGhoYzhvOHlhdzdt"
               target="_blank"
               rel="noopener noreferrer"
               className="underline"
@@ -29,7 +24,7 @@ const FooterBottom = () => {
               Instagram
             </a>
             <a
-              href="https://linkedin.com"
+              href="https://www.linkedin.com/company/musicspeaks/"
               target="_blank"
               rel="noopener noreferrer"
               className="underline"
@@ -39,24 +34,14 @@ const FooterBottom = () => {
           </div>
         </div>
 
-        {/* Middle Column */}
-        <div className="flex items-center justify-center order-first lg:order-none mb-8 lg:mb-0">
-          <a
-            href="#"
-            className="bg-[#082FFF] text-white px-6 sm:px-8 py-2 sm:py-3 rounded-full font-medium text-base sm:text-lg hover:bg-[#0626CC] transition-colors duration-300"
-          >
-            book a call
-          </a>
-        </div>
-
         {/* Right Column */}
         <div className="flex flex-col items-center lg:items-end space-y-4">
           <a href="/privacy-policy">Privacy Policy</a>
           <a href="/terms-of-use">Terms of Use</a>
         </div>
 
-        {/* Credits - Full width on mobile */}
-        <div className="col-span-1 lg:col-span-3 text-center mt-8">
+        {/* Credits - Full width */}
+        <div className="col-span-1 lg:col-span-2 text-center mt-8">
           <p className="text-white text-xs">
             Developed with 😍 by{" "}
             <a href="https://belowthefold.gr/" className="text-yellow-300">
